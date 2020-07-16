d5e6d993a7f3927dc089270c562f95082746cc0e
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-07-16 21:15:37 +0530

    Add asp.net core project.
    Add asp.net core project and write some code to show that
    session state is non-locking for asp.net core. Check the output
    in the Output.txt file.

---
+++ SessionLock.AspnetCore/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SessionLock.AspnetCore.Models;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SessionLock.AspnetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string userName)
+        {
+            if (!string.IsNullOrWhiteSpace(userName))
+            {
+                HttpContext.Session.SetString("UserName", userName);
+                string uName = HttpContext.Session.GetString("UserName");
+
+                for (var i = 0; i < 10; i++)
+                {
+                    _logger.LogInformation($"{HttpContext.Session.Id} {uName}");
+                    Thread.Sleep(1000);
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SessionLock.AspnetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SessionLock.AspnetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
