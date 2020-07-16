55fca9a661b169b04fd8fef42837b5df7b99c519
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-07-16 21:44:22 +0530

    Add asp.net framework project.
    Add asp.net framework project and write some code to show that
    session state is locking for asp.net framework. If requests come from
    same client then it process those one by one. Check the output
    in the Output.txt file.

---
+++ SessionLock.AspnetFramework/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Web.Mvc;
+using System.Web.SessionState;
+
+namespace SessionLock.AspnetFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string userName)
+        {
+            if (!string.IsNullOrWhiteSpace(userName))
+            {
+                Session["UserName"] = userName;
+                string uName = (string)Session["UserName"];
+
+                for (var i = 0; i < 10; i++)
+                {
+                    Trace.WriteLine($"{Session.SessionID} {uName}");
+                    Thread.Sleep(1000);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
